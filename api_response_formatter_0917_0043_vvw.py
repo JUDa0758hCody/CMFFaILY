87f3c264a12327829894eab981f2cfce2dd6aa4c
JUDa0758hCody <bends_nascent0s@icloud.com>
2025-09-16 18:43:45 +0200

    Update API响应格式化工具

---
+++ api_response_formatter_0917_0043_vvw.py
@@ -0,0 +1,16 @@
+# 代码生成时间: 2025-09-17 00:43:44
+import json
+from gradio import Interface, Text, JSON
+
+"""
+API响应格式化工具
+
+这个工具接受JSON格式的API响应，对其进行格式化，并以更易读的方式展示。
+
+功能：
+- 接受JSON格式的API响应
+- 格式化响应以便更易读
+"""
+
+def format_api_response(api_response):
+    "
